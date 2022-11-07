3c4b4cab25aaaa106f5f9455da180f3ca5267ad2
Jacob Hands <jhands@cloudflare.com>
2022-11-07 13:58:22 -0600

    TEsts

--- test/tests/index.test.ts
+++ test/tests/index.test.ts
@@ -1,10 +1,28 @@
 import { describe, expect, it } from "vitest";
 import fetch from "node-fetch";
+const host = "https://js-example-1.pages-examples.pages.dev";
+
+async function get(path: string): Promise<string> {
+  const res = await fetch(`${host}${path}`, {});
+  return await res.text();
+}
+
 // The two tests marked with concurrent will be run in parallel
-describe("suite", () => {
-  it("serial test", async () => {
-    const res = await fetch("https://uuid.rocks/plain", {});
-    const text = await res.text();
-    expect(text.length).toBe(36);
+describe("pages", () => {
+  it("should serve both /foo and /foo/", async () => {
+    expect(await get("/")).toMatchInlineSnapshot('"/functions/index.js"');
+    expect(await get("")).toMatchInlineSnapshot('"/functions/index.js"');
+
+    expect(await get("/helloworld")).toMatchInlineSnapshot('"/functions/helloworld.js"');
+    expect(await get("/helloworld/")).toMatchInlineSnapshot('"/functions/helloworld.js"');
+
+    expect(await get("/fruits/apple")).toMatchInlineSnapshot('"/functions/fruits/apple.js"');
+    expect(await get("/fruits/apple/")).toMatchInlineSnapshot('"/functions/fruits/apple.js"');
+
+    expect(await get("/fruits/banana")).toMatchInlineSnapshot('"/functions/fruits/banana.js"');
+    expect(await get("/fruits/banana/")).toMatchInlineSnapshot('"/functions/fruits/banana.js"');
+
+    expect(await get("/fruits")).toMatchInlineSnapshot('"/functions/fruits/index.js"');
+    expect(await get("/fruits/")).toMatchInlineSnapshot('"/functions/fruits/index.js"');
   });
 });
